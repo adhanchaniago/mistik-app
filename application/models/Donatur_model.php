0daa95bcb04ab38267f39e3e1b2f7f0a72ae63ab
Hexated <37908684+hexated@users.noreply.github.com>
2018-12-12 12:36:31 +0700

    initial commit

--- application/models/Donatur_model.php
+++ application/models/Donatur_model.php
@@ -13,7 +13,7 @@ class Donatur_model extends CI_model
 
             "username" => $this->input->post('username', true),
             "email" => $this->input->post('email', true),
-            "password" => $this->input->post(md5('password'), true),
+            "password" => md5($this->input->post('password', true)),
             "alamat" => $this->input->post('alamat', true),
             "no_telepon" => $this->input->post('notelp', true)
 
--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -7,7 +7,7 @@ class Login_model extends CI_Model
         // $check = $this->db->get_where('donatur', array('username' => $username, 'password' => $password));
 
         $this->db->where('username', $username);
-        $this->db->where('password', $password);
+        $this->db->where('password', md5($password));
         $query = $this->db->get('donatur');
 
         if ($query->num_rows() > 0) {
